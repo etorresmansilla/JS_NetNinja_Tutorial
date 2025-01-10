89d5306fdf8f52273649927843e1868e878e3fc7
etorresmansilla <etorresmansilla@rrc.ca>
2025-01-10 10:59:36 -0600

    commit #5: callbacks & forEach

--- sandbox.js
+++ sandbox.js
@@ -444,4 +444,45 @@ console.log(resultOne);
 let resultTwo = name.toUpperCase();
 console.log(resultTwo);
 
+
+
+// ! callbacks & foreach
+
+// const myFunc = (callbackFunc) => {
+//     // do something
+//     let value = 50;
+//     callbackFunc(value);
+// };
+
+// myFunc(function(value){
+//     // do something
+//     console.log(value);
+// });
+
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+const logPerson = (person, index) => {
+    console.log(`${index} - hello ${person}`);
+};
+
+people.forEach((person, index) => {
+    console.log(index, person);
+});
+
+people.forEach(logPerson);
 */
+// ! get a reference to the 'ul'
+
+const ul = document.querySelector('.people');
+
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+let html = ``;
+
+people.forEach(person => {
+    // create an html template
+    html += `<li style="color: purple">${person}</li>`;
+});
+
+console.log(html);
+ul.innerHTML = html;
