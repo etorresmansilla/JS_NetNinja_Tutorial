903d16991a7716dd5272f2ab4bb7b40c797236a2
etorresmansilla <etorresmansilla@rrc.ca>
2025-01-10 14:31:12 -0600

    commit #7: Math object

--- sandbox.js
+++ sandbox.js
@@ -577,4 +577,28 @@ let user = {
 user.logBlogs();
 user.login();
 user.logout();
+
+
+
+// ! math object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+// random numbers
+
+const random = Math.random();
+// random is between 0 and 1
+
+console.log(random);
+console.log(Math.round(random * 100));
 */
+
+// ! primitive types & reference types
